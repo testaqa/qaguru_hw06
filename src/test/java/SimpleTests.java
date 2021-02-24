45a7ca2bfccebead4512d79e7b4015e1516997b3
Vasiliy Golban <vgolban@accesssoftek.com>
2021-02-24 11:48:24 +0200

    Add SearchGoogle test

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,12 +1,16 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleTests {
 
     @Test
-    void FirstTest() {
-        assertTrue(true);
+    void SearchGoogle() {
+        open("https://google.com/");
+        $("[name=q]").val("Selenide").pressEnter();
+
+        $$("a cite").first().shouldHave(text("selenide.org"));
     }
 
 }
